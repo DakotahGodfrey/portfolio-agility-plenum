6d5890e7c9c67dfe08a179f08cf889c799bf0ecc
Joel Varty <joel@agilitycms.com>
2021-03-25 11:39:25 -0400

    logs

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -63,7 +63,7 @@ export async function getStaticProps({ preview, params, locale, defaultLocale, l
 		if (defaultLocale === undefined) defaultLocale = null
 		if (defaultLocale === undefined) defaultLocale = null
 
-		const root = path.resolve(process.cwd(), "cache")
+		const root = "./cache"
 
 		traverse(root, 0)
 
