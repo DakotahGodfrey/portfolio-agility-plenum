ecc88cda2331906442310bca564c99c2aadefc16
Joel Varty <joel@agilitycms.com>
2021-04-06 15:08:15 -0400

    webpack config

--- next.config.js
+++ next.config.js
@@ -10,5 +10,15 @@ module.exports = {
 		'pages/index.tsx': {
 			includeDirs: ['./.next/cache/agility']
 		}
-	}
+	},
+	webpack: (config, { isServer }) => {
+		// Fixes npm packages that depend on `fs` module
+		if (!isServer) {
+		  config.node = {
+			fs: 'empty'
+		  }
+		}
+
+		return config
+	  }
 }
