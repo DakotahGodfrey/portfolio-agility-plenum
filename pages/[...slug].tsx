e06bc9f128fd9bb21625bbd1c659f06cd9a3574e
Joel Varty <joel@agilitycms.com>
2021-04-21 16:12:59 -0400

    removed console

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -21,61 +21,13 @@ import path from "path"
 
 export async function getStaticProps({ preview, params, locale, defaultLocale, locales }: GetStaticPropsContext<{ slug: string[] }>) {
 
-
-
-const traverse = (rootPath, depth) => {
-
-	if (depth > 0) return
-
-	if (!existsSync(rootPath)) {
-		console.log(`${rootPath} does not exist`)
-		return
-	}
-
-	try {
-		const listing = readdirSync(rootPath, { withFileTypes: true })
-
-		let indent = " "
-		for (let i = 0; i < depth; i++) indent += " "
-
-		//listing all files using forEach
-		listing.forEach(sub => {
-
-			if (sub.isDirectory()) {
-				console.log(`${indent} * ${sub.name}`);
-				traverse(path.resolve(rootPath, sub.name), depth + 1)
-			} else {
-				console.log(`${indent} - ${sub.name}`);
-			}
-		})
-
-	} catch (e) {
-		return
-	}
-}
-
-
-	const testPath = path.join(process.cwd(),  ".next/cache/agility")
-
-	console.log("Traverse: ", testPath)
-	console.log("VERCEL:", process.env.VERCEL, "NETLIFY:", process.env.SITE_ID)
-
-	traverse(testPath, 0)
-
-
 	try {
 		const globalComponents = {
 			"header": GlobalHeader,
 			"footer": GlobalFooter
 		}
 
-		// if (params === undefined) params = null
-		// if (preview === undefined) preview = false
-		// if (locale === undefined) locale = null
-		// if (defaultLocale === undefined) defaultLocale = null
-
 		const agilityProps = await getAgilityPageProps({ preview, params, locale, getModule, defaultLocale, globalComponents });
-
 
 		let rebuildFrequency = 10
 
