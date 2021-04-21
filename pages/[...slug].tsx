ad380e84a2e05bf405c57f6574d9164ba9c9d926
Joel Varty <joel@agilitycms.com>
2021-04-21 14:58:38 -0400

    env tests

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -58,6 +58,7 @@ const traverse = (rootPath, depth) => {
 	const testPath = path.join(process.cwd(),  "agility")
 
 	console.log("Traverse: ", testPath)
+	console.log("VERCEL:", process.env.VERCEL, "NETLIFY:", process.env.NETLIFY)
 
 	traverse(testPath, 0)
 
