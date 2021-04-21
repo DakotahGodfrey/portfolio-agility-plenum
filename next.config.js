9f30ec2dd882c14ca262ecd9535f9fbfd8c2a77c
Joel Varty <joel@agilitycms.com>
2021-04-21 09:25:05 -0400

    config

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	target: "serverless",
+	future: {
+		webpack5: false,
+	},
+	unstableNetlifyFunctionsSupport: {
+		'pages/[...slug].tsx': {
+			includeDirs: ['./.next/cache/agility']
+		},
+		'pages/index.tsx': {
+			includeDirs: ['./.next/cache/agility']
+		}
+	}
+}
