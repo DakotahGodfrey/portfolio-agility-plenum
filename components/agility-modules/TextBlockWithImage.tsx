ad75b116adbc0ef117ea11d82f25d472c520791e
Dakotah Godfrey <dakotah.godfrey.dev>
2022-10-09 15:38:36 -0400

    init commit

---
+++ components/agility-modules/TextBlockWithImage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TextBlockWithImage: React.FC = () => {
+  return <div>TextBlockWithImage</div>;
+};
+
+export default TextBlockWithImage;
--- components/agility-modules/index.ts
+++ components/agility-modules/index.ts
@@ -1,26 +1,29 @@
-import RichTextArea from "./RichTextArea"
-import HeroBackgroundAsImage from "./HeroBackgroundAsImage"
-import PostsListing from "./PostsListing"
-import PostDetails from "./PostDetails"
+import RichTextArea from "./RichTextArea";
+import HeroBackgroundAsImage from "./HeroBackgroundAsImage";
+import PostsListing from "./PostsListing";
+import PostDetails from "./PostDetails";
+import TextBlockWithImage from "./TextBlockWithImage";
 
 /**
  * All of the Agility Module components that are in use in this site.
  */
 const allModules = [
-	{ name: "HeroBackgroundAsImage", module: HeroBackgroundAsImage },
-	{ name: "PostsListing", module: PostsListing },
-	{ name: "PostDetails", module: PostDetails },
-	{ name: "RichTextArea", module: RichTextArea }
-]
+  { name: "HeroBackgroundAsImage", module: HeroBackgroundAsImage },
+  { name: "PostsListing", module: PostsListing },
+  { name: "PostDetails", module: PostDetails },
+  { name: "RichTextArea", module: RichTextArea },
+  { name: "TextBlockWithImage", module: TextBlockWithImage },
+];
 
 /**
  * Find the component for a module by name.
  * @param moduleName
  */
 export const getModule = (moduleName: string): any | null => {
-
-	if (! moduleName) return null
-	const obj = allModules.find(m => m.name.toLowerCase() === moduleName.toLowerCase())
-	if (!obj) return null
-	return obj.module
-}
+  if (!moduleName) return null;
+  const obj = allModules.find(
+    (m) => m.name.toLowerCase() === moduleName.toLowerCase()
+  );
+  if (!obj) return null;
+  return obj.module;
+};
